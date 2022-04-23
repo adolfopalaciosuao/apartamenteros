7136b20a91f7499805b8ef1657ea1108112d36d9
JCGS1 <100473213+JCGS1@users.noreply.github.com>
2022-04-23 11:57:48 -0500

    Interfaz completa

--- Assets/JCScripts/CerrarVideo.cs
+++ Assets/JCScripts/CerrarVideo.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+//
+
+using UnityEngine.Video;
 
 public class CerrarVideo : MonoBehaviour
 {
+
+    public VideoPlayer video;
     // Start is called before the first frame update
+
+    void Awake()
+
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+
+    }
+
+ 
     void Start()
     {
         
@@ -13,6 +30,12 @@ public class CerrarVideo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(6);
     }
 }
