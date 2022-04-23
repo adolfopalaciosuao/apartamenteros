70a2d2f4c92d10935f22b225beaa443f93debe7e
JCGS1 <100473213+JCGS1@users.noreply.github.com>
2022-04-23 13:07:25 -0500

    parcialmente terminado

---
+++ Assets/Scripts/robotScript.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotScript : MonoBehaviour
+{
+
+    public TextMesh textoFeedback;
+    public string msgInteraccion;
+
+    public CharacterController player;
+
+    public GameObject Robot;
+    public bool isOnRobot;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetTextMessage(msgInteraccion, false);
+        Debug.Log(Input.GetJoystickNames());
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(isOnRobot)
+        {
+
+            if (Input.GetButtonDown("Fire2"))
+            {
+                Debug.Log("Toca B");
+                SetTextMessage("Aparece cuadro", true);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = other.gameObject.GetComponent<CharacterController>();
+
+            if (player != null)
+            {
+                //Entro un player
+                isOnRobot = true;
+                SetTextMessage(msgInteraccion, true);
+               
+
+            }
+        }
+        Debug.Log("Entro");
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<CharacterController>() == player)
+        {
+
+            //Robot.GetComponent<Animator>().SetBool("OpenChest", false);
+            isOnRobot = false;
+            player = null;
+            SetTextMessage(msgInteraccion, false);
+
+
+        }
+    }
+
+    void SetTextMessage(string message, bool isActive)
+    {
+        textoFeedback.text = message;
+        textoFeedback.gameObject.SetActive(isActive);
+    }
+}
